565975aa62fafccb5fd052f12f030b6318a09a8e
amit199915 <1999guptaamit@gmail.com>
2022-02-08 13:10:05 +0530

    first commit

---
+++ frontend-app/src/amit-ui-task/script.js
@@ -0,0 +1,13 @@
+
+
+function loginmodal (){
+    document.getElementById('login-modal').style.display="block"; 
+}
+
+function closemodal (){
+    document.getElementById('login-modal').style.display="none"; 
+    document.getElementById('signup-modal').style.display="none"; 
+}
+function signupModal (){
+    document.getElementById('signup-modal').style.display="block"; 
+}
